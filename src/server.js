1ed7d73f5f2780f7ae7a142347adea7731fa6b05
choibyungjae <shapa355@naver.com>
2021-11-24 21:42:25 +0900

    2021.11.24
    mongodb, mongoose

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -6,14 +8,13 @@ import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
-console.log(process.cwd());
-
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({extended:true}));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
