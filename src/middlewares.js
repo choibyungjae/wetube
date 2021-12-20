a54b1e8f6676e487b1e6fc950d77df7a2ee9cae1
choibyungjae <shapa355@naver.com>
2021-12-20 18:51:23 +0900

    Update middlewares.js

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,15 @@ const s3 = new aws.S3({
     }
 });
 
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
     s3: s3,
-    bucket: 'wetubebyungjae',
+    bucket: 'wetubebyungjae/images',
+    //acl: "public-read",
+});
+
+const s3videoUploader = multerS3({
+    s3: s3,
+    bucket: 'wetubebyungjae/videos',
     //acl: "public-read",
 });
 
@@ -45,7 +51,7 @@ export const avatarUpload = multer({
     limits: {
         fileSize: 3000000,
     },
-    storage: multerUploader
+    storage: s3ImageUploader
 });
 
 export const videoUpload = multer({
@@ -53,5 +59,5 @@ export const videoUpload = multer({
     limits: {
         fileSize: 100000000,
     },
-    storage: multerUploader
+    storage: s3videoUploader
 });
